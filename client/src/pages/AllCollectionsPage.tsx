39d461a10997c5657d327e94f00a1d2038477b49
Darksller <darksller.sss@gmail.com>
2024-01-26 05:11:13 +0300

    fix: added loading to collections page

--- client/src/pages/AllCollectionsPage.tsx
+++ client/src/pages/AllCollectionsPage.tsx
@@ -2,17 +2,20 @@ import { useGetCollectionsQuery } from '@/api/collectionsApi'
 import { CollectionCard } from '@/components/ui/collections/collection-card'
 
 export function Collections() {
-  const { data } = useGetCollectionsQuery()
+  const { data, isLoading } = useGetCollectionsQuery()
 
-  if (!data) return <h1 className="text-4xl">No collections was found</h1>
+  if (data && data?.length === 0)
+    return <h1 className="text-4xl">No collections was found</h1>
   return (
     //TODO: Карусель по темам
     <div className="mt-12 h-svh max-w-1440 rounded-xl backdrop-blur dark:bg-white dark:bg-opacity-40">
-      <div className="flex flex-wrap gap-4 ">
-        {data.map((collection) => (
-          <CollectionCard key={collection._id} collection={collection} />
-        ))}
-      </div>
+      {!isLoading && (
+        <div className="flex flex-wrap gap-4 ">
+          {data?.map((collection) => (
+            <CollectionCard key={collection._id} collection={collection} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
