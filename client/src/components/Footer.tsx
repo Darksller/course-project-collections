db9aae5b828d873ec42570eb69f85093dea801e6
Darksller <darksller.sss@gmail.com>
2024-01-28 02:57:20 +0300

    dev init

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,6 +4,7 @@ export function Footer() {
   return (
     <div className="flex h-[56px] w-full items-center bg-pink-600 bg-white/60 px-6 font-muli text-pink-600/100 shadow-2xl backdrop-blur dark:bg-pink-600/50 dark:text-white">
       <LanguageSelect />
+      <div>das</div>
     </div>
   )
 }
