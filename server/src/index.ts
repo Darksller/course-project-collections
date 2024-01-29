d01c5a05454ca80abea080b71eb1ddf3b72df3d7
Darksller <darksller.sss@gmail.com>
2024-01-29 06:42:06 +0300

    fix: added same setting to io server

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,16 @@ app.use(bodyParser.json())
 
 const server = http.createServer(app)
 
-export const io = new Server(server)
+export const io = new Server(server, {
+	cors: {
+		origin: [
+			'http://localhost:5173',
+			'https://dunk-vault.vercel.app',
+			'https://dunk-vault-git-dev-darksllers-projects.vercel.app/',
+		],
+		allowedHeaders: ['Access-Control-Allow-Origin: *'],
+	},
+})
 
 io.on('connection', socket => {
 	socket.on('joinRoom', itemId => {
